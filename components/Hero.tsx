2a77c10f00548a85eb48d3c6f0c82402ad494466
beauverger <beauverger.rodolphe@orange.fr>
2022-10-10 15:23:58 +0200

    test deploy change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero({ pageInfo }: Props) {
       <BackgroundCircles />
       <img className='relative rounded-full h-32 w-32 mx-auto object-cover' src={urlFor(pageInfo?.heroImage).url()} alt={`${pageInfo?.name}`} />
       <div className='z-20'>
-        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>{pageInfo?.role}</h2>
+        <h2 className='text-sm uppercase text-orange-500 pb-2 tracking-[15px]'>{pageInfo?.role}</h2>
          <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
           <span className='mr-3'>{text}</span>
           <Cursor cursorColor="#F7AB0A" />
